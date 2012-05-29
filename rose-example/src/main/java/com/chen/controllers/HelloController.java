8441765c4a7cdeebbe20a446786d3867223c79d7
陈臻 <czhttp@gmail.com>
2012-05-29 17:33:26 +0800

    update chapter 3.6

--- rose-example/src/main/java/com/chen/controllers/HelloController.java
+++ rose-example/src/main/java/com/chen/controllers/HelloController.java
@@ -6,9 +6,11 @@ package com.chen.controllers;
 
 import com.chen.model.Chen;
 
+import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.annotation.Param;
 import net.paoding.rose.web.annotation.Path;
 import net.paoding.rose.web.annotation.rest.Get;
+import net.paoding.rose.web.var.Flash;
 
 @Path("")
 public class HelloController {
@@ -31,6 +33,18 @@ public class HelloController {
     @Get("/notBlank")
     public String notBlank(@NotBlank @Param("messages") String messages) throws Exception {
         return "@hello world";
+    }
+
+    @Get("/flash1")
+    public String flashStep1(Flash flash) {
+        flash.add("msg", "修改成功！");
+        return "r:/flash2";
+    }
+
+    @Get("/flash2")
+    public String flashStep2(Invocation inv, Flash flash) {
+        inv.addModel("info", flash.get("msg"));
+        return "flash";
     }
 }
 
---
+++ rose-example/src/main/webapp/views/flash.jsp
@@ -0,0 +1,2 @@
+<%@ page contentType="text/html;charset=UTF-8"%>
+提示信息：${info}
