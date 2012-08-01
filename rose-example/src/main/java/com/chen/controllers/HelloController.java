9f203b4558a0e43b1d1e337ac78c302c7d3619f5
陈臻 <czhttp@gmail.com>
2012-08-01 19:44:38 +0800

    update test case

--- rose-example/src/main/java/com/chen/controllers/HelloController.java
+++ rose-example/src/main/java/com/chen/controllers/HelloController.java
@@ -2,11 +2,15 @@
  * @author 54chen(陈臻) [chenzhen@xiaomi.com czhttp@gmail.com]
  * @since 2012-4-10 上午11:14:46
  */
+
 package com.chen.controllers;
 
 import com.chen.model.Chen;
 import com.chen.model.Test;
 import com.chen.service.TestService;
+import com.netflix.curator.framework.CuratorFramework;
+import com.netflix.curator.framework.CuratorFrameworkFactory;
+import com.netflix.curator.retry.RetryNTimes;
 
 import net.paoding.rose.web.Invocation;
 import net.paoding.rose.web.annotation.Param;
@@ -17,6 +21,9 @@ import net.paoding.rose.web.portal.Pipe;
 import net.paoding.rose.web.portal.Portal;
 import net.paoding.rose.web.var.Flash;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -104,5 +111,63 @@ public class HelloController {
         String s = "Hello the No." + t.getId() + " is " + t.getMsg();
         return "@" + s;
     }
-}
 
+    @Get("/curator")
+    public String testCurator() throws Exception {
+        String path = "/test_path";
+        CuratorFramework client = CuratorFrameworkFactory.builder().connectString("zookeeper.n.miliao.com:2181").namespace("/test1")
+                .retryPolicy(new RetryNTimes(Integer.MAX_VALUE, 1000)).connectionTimeoutMs(5000).build();
+        // create a node
+        client.create().forPath("/head", new byte[0]);
+
+        // delete a node in background
+        client.delete().inBackground().forPath("/head");
+
+        // create a EPHEMERAL_SEQUENTIAL
+        client.create().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath("/head/child", new byte[0]);
+
+        // get the data
+        client.getData().watched().inBackground().forPath("/test");
+
+        // check the path exits
+        client.checkExists().forPath(path);
+
+        return "@adf";
+    }
+
+    public static void main(String[] args) throws Exception {
+        String path = "/test_path";
+        CuratorFramework client = CuratorFrameworkFactory.builder().connectString("zookeeper.n.miliao.com:2181").namespace("/brokers").retryPolicy(new RetryNTimes(Integer.MAX_VALUE, 1000)).connectionTimeoutMs(5000).build();
+        // 启动 上面的namespace会作为一个最根的节点在使用时自动创建
+        client.start();
+
+        // 创建一个节点
+        client.create().forPath("/head", new byte[0]);
+
+        // 异步地删除一个节点
+        client.delete().inBackground().forPath("/head");
+
+        // 创建一个临时节点
+        client.create().withMode(CreateMode.EPHEMERAL_SEQUENTIAL).forPath("/head/child", new byte[0]);
+
+        // 取数据
+        client.getData().watched().inBackground().forPath("/test");
+
+        // 检查路径是否存在
+        client.checkExists().forPath(path);
+
+        // 异步删除
+        client.delete().inBackground().forPath("/head");
+
+        // 注册观察者，当节点变动时触发
+        client.getData().usingWatcher(new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("node is changed");
+            }
+        }).inBackground().forPath("/test");
+
+        // 结束使用
+        client.close();
+    }
+}
